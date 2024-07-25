c506a3c248feac53fb338cbe8f0ef4d9fb8d3c30
Felipe Martinelli <felipemmartinelli@hotmail.com>
2024-07-25 01:30:05 -0300

    Consulta de extrato e saldo

--- Genial.CashFlow.Domain/Services/TransactionService.cs
+++ Genial.CashFlow.Domain/Services/TransactionService.cs
@@ -2,6 +2,8 @@
 using Genial.CashFlow.Application.Dtos.Commands;
 using Genial.CashFlow.Application.Dtos.Queries;
 using Genial.CashFlow.Infrastructure.Repositories;
+using Genial.Framework.Exceptions;
+using Genial.Framework.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,12 +23,46 @@ namespace Genial.CashFlow.Domain.Services
 
         public async Task<GetStatementQueryResult> GetStatementAsync(GetStatementQuery request)
         {
-            return await this.transactionRepository.GetStatementAsync(request);
+            request.CustomerDocument = request.CustomerDocument.OnlyNumbers()?.PadLeft(11, '0')!;
+            request.AgencyNumber = request.AgencyNumber.OnlyNumbers()?.PadLeft(4, '0')!;
+            request.AccountNumber = request.AccountNumber.OnlyNumbers()?.PadLeft(6, '0')!;
+
+            if (request.StartDate is not null)
+                request.StartDate = request.StartDate.Value.Date;
+
+            if (request.EndDate is not null)
+                request.EndDate = request.EndDate.Value.Date.Add(new TimeSpan(23, 59, 59));
+
+
+            var result = await this.transactionRepository.GetStatementAsync(request);
+
+
+            if (result.Customer is null)
+                throw new NotFoundBusinessException("Cliente não encontrado.");
+
+            if (result.Account is null)
+                throw new NotFoundBusinessException("Conta Corrente não encontrada para esse cliente.");
+
+            return result;
         }
 
         public async Task<GetBalanceQueryResult> GetBalanceAsync(GetBalanceQuery request)
         {
-            return await this.transactionRepository.GetBalanceAsync(request);
+            request.CustomerDocument = request.CustomerDocument.OnlyNumbers()?.PadLeft(11, '0')!;
+            request.AgencyNumber = request.AgencyNumber.OnlyNumbers()?.PadLeft(4, '0')!;
+            request.AccountNumber = request.AccountNumber.OnlyNumbers()?.PadLeft(6, '0')!;
+
+
+            var result = await this.transactionRepository.GetBalanceAsync(request);
+
+
+            if (result.Customer is null)
+                throw new NotFoundBusinessException("Cliente não encontrado.");
+
+            if (result.Account is null)
+                throw new NotFoundBusinessException("Conta Corrente não encontrada para esse cliente.");
+
+            return result;
         }
 
 
--- Genial.CashFlow.Infrastructure/Repositories/TransactionRepository.cs
+++ Genial.CashFlow.Infrastructure/Repositories/TransactionRepository.cs
@@ -59,46 +59,30 @@ namespace Genial.CashFlow.Infrastructure.Repositories
                 t.[Description],
                 t.[Value],
                 t.[BalanceValue]
-
-                FROM [Customers] AS c 
-                INNER JOIN [Accounts] AS a ON a.[CustomerId] = c.Id
-                INNER JOIN [Transactions] AS t ON t.[AccountId] = a.Id
+            FROM [Customers] AS c 
+            INNER JOIN [Accounts] AS a ON a.[CustomerId] = c.Id
+            INNER JOIN [Transactions] AS t ON t.[AccountId] = a.Id
             WHERE
                 c.[Document] = @CustomerDocument AND
                 a.[AgencyNumber] = @AgencyNumber AND
                 a.[Number] = @AccountNumber";
 
-            
-            request.CustomerDocument = request.CustomerDocument.OnlyNumbers()?.PadLeft(11, '0')!;
-            request.AgencyNumber = request.AgencyNumber.OnlyNumbers()?.PadLeft(4, '0')!;
-            request.AccountNumber = request.AccountNumber.OnlyNumbers()?.PadLeft(6, '0')!;
 
             if (request.StartDate is not null)
-            {
-                request.StartDate = request.StartDate.Value.Date;
                 sql += " AND t.[Date] >= @StartDate";
-            }
+
             if (request.EndDate is not null)
-            {
-                request.EndDate = request.EndDate.Value.Date.Add(new TimeSpan(23, 59, 59));
                 sql += " AND t.[Date] <= @EndDate";
-            }
 
 
             using var gridReader = await connection.QueryMultipleAsync(sql, request);
 
             var result = new GetStatementQueryResult()
             {
-                Customer = gridReader.Read<CustomerDto>().FirstOrDefault()!,
-                Account = gridReader.Read<AccountDto>().FirstOrDefault()!,
-                Transactions = gridReader.Read<TransactionDto>().ToList()
+                Customer = (await gridReader.ReadFirstOrDefaultAsync<CustomerDto>())!,
+                Account = (await gridReader.ReadFirstOrDefaultAsync<AccountDto>())!,
+                Transactions = (await gridReader.ReadAsync<TransactionDto>()).ToList()
             };
-
-            if (result.Customer is null)
-                throw new NotFoundBusinessException("Cliente não encontrado.");
-
-            if (result.Account is null)
-                throw new NotFoundBusinessException("Conta Corrente não encontrada.");
 
             return result;
         }
@@ -108,77 +92,48 @@ namespace Genial.CashFlow.Infrastructure.Repositories
             using var connection = this.dapperContext.CreateConnection();
 
             var sql = @"
-                SELECT 
-                    c.[Id], 
-                    c.[Name],
-                    c.[Document],
+            SELECT 
+                c.[Id], 
+                c.[Name],
+                c.[Document]
+            FROM [Customers] AS c
+            WHERE
+                c.[Document] = @CustomerDocument;
 
-                    a.[Id], 
-                    a.[AgencyNumber],
-                    a.[Number],
-
-                    t.[Id],
-                    t.[Date],
-                    t.[Type],
-                    t.[Description],
-                    t.[Value],
-                    t.[BalanceValue]
-
-                FROM [Customers] AS c 
-                LEFT JOIN [Accounts] AS a ON a.[CustomerId] = c.Id
-                LEFT JOIN [Transactions] AS t ON t.[AccountId] = a.Id
+            SELECT 
+                a.[Id], 
+                a.[AgencyNumber],
+                a.[Number]
+            FROM [Customers] AS c 
+            INNER JOIN [Accounts] AS a ON a.[CustomerId] = c.Id
             WHERE
                 c.[Document] = @CustomerDocument AND
                 a.[AgencyNumber] = @AgencyNumber AND
-                a.[Number] = @AccountNumber";
+                a.[Number] = @AccountNumber;
+
+            SELECT TOP 1
+                t.[Id],
+                t.[BalanceValue]
+            FROM [Customers] AS c 
+            INNER JOIN [Accounts] AS a ON a.[CustomerId] = c.Id
+            INNER JOIN [Transactions] AS t ON t.[AccountId] = a.Id
+            WHERE
+                c.[Document] = @CustomerDocument AND
+                a.[AgencyNumber] = @AgencyNumber AND
+                a.[Number] = @AccountNumber
+            ORDER BY t.[Id] DESC";
 
 
-            request.CustomerDocument = request.CustomerDocument.OnlyNumbers()?.PadLeft(11, '0')!;
-            request.AgencyNumber = request.AgencyNumber.OnlyNumbers()?.PadLeft(4, '0')!;
-            request.AccountNumber = request.AccountNumber.OnlyNumbers()?.PadLeft(6, '0')!;
+            using var gridReader = await connection.QueryMultipleAsync(sql, request);
 
+            var result = new GetBalanceQueryResult()
+            {
+                Customer = (await gridReader.ReadFirstOrDefaultAsync<CustomerDto>())!,
+                Account = (await gridReader.ReadFirstOrDefaultAsync<AccountDto>())!,
+                BalanceValue = (await gridReader.ReadFirstOrDefaultAsync<TransactionDto>())?.BalanceValue ?? 0
+            };
 
-
-
-            var resultLookup = new Dictionary<long, GetBalanceQueryResult>();
-
-            var results = await connection
-                .QueryAsync(
-                    sql,
-                    [
-                        typeof(CustomerDto),
-                        typeof(AccountDto),
-                        typeof(TransactionDto)
-                    ],
-                    obj =>
-                    {
-                        var customer = (CustomerDto)obj[0];
-                        var account = (AccountDto)obj[1];
-                        var transaction = (TransactionDto)obj[2];
-                        GetBalanceQueryResult? result;
-
-                        if (!resultLookup.TryGetValue(account.Id, out result))
-                        {
-                            result = new GetBalanceQueryResult()
-                            {
-                                Customer = customer,
-                                Account = account
-                            };
-
-                            resultLookup.Add(account.Id, result);
-                        }
-
-                        if (transaction is not null)
-                            result.BalanceValue = transaction.BalanceValue;
-
-                        return result;
-                    },
-                    request);
-
-            if (!results.Any())
-                throw new NotFoundBusinessException("Cliente ou conta corrente não encontrados.");
-
-            return results.First();
+            return result;
         }
     }
 }
